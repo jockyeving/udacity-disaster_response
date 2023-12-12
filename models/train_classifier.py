10b78390d42282259a49ec3d849ea505bddd1dc9
root <levente.kovacs2@zf.com>
2023-12-12 15:36:33 +0000

    changing pipeline to simpler form due to better result.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -24,25 +24,6 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-
-    def starting_verb(self, text):
-        sentence_list = nltk.sent_tokenize(text)
-        for sentence in sentence_list:
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-            first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                return True
-        return False
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
-
-
 def load_data(database_filepath):
     # fetching data from SQL
     engine = create_engine('sqlite:///' + database_filepath)
@@ -67,17 +48,9 @@ def tokenize(text):
 def build_model():
     # defining the ML pipeline
     pipeline = Pipeline([
-        ('features', FeatureUnion([
-            
-            ('text_pipeline', Pipeline([
-                ('vect', CountVectorizer(tokenizer=tokenize)),
-                ('tfidf', TfidfTransformer())
-            ])),
-
-            ('starting_verb', StartingVerbExtractor())
-        ])),
-    
-        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier())),
     ])
     
     # defining parameters for GridSearch
